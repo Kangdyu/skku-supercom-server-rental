7c577ab25531e67c6e54f89b77d3fa5a868df984
Kangdyu <kangdyu99@gmail.com>
2023-07-13 16:14:23 +0900

    feat: app -> pages / antd -> mantine / mysql -> postgres

---
+++ src/pages/_app.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+import NextApp, { AppProps, AppContext } from 'next/app';
+import Head from 'next/head';
+import { MantineProvider, ColorScheme, ColorSchemeProvider } from '@mantine/core';
+
+export default function App(props: AppProps & { colorScheme: ColorScheme }) {
+  const { Component, pageProps } = props;
+
+  return (
+    <>
+      <Head>
+        <title>Mantine next example</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        <link rel="shortcut icon" href="/favicon.svg" />
+      </Head>
+      <MantineProvider withGlobalStyles withNormalizeCSS>
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,6 @@
+import 'dayjs/locale/ko';
+import { DatePickerInput } from '@mantine/dates';
+
+export default function MantinePage() {
+  return <DatePickerInput locale="ko" />;
+}
