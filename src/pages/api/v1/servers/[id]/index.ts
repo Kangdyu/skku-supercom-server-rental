36a704dc49a3a0f4d02db22783fbc3da358127a4
Kangdyu <kangdyu99@gmail.com>
2023-07-14 16:54:29 +0900

    feat: GET, PUT /api/v1/servers/:id

---
+++ src/pages/api/v1/servers/[id]/index.ts
@@ -0,0 +1,57 @@
+import { handleApiError } from '@/lib/errorHandlers';
+import prisma from '@/lib/prisma';
+import { ServerDTO } from '@/types/api';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+/**
+ * GET /api/v1/servers/:id
+ */
+async function getServer(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const { id } = req.query;
+
+    const server = await prisma.server.findUnique({
+      where: {
+        id: Number(id),
+      },
+    });
+
+    return res.status(200).json({ server });
+  } catch (e) {
+    handleApiError(e, res);
+  }
+}
+
+/**
+ * PUT /api/v1/servers/:id
+ */
+async function putServer(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const { id } = req.query;
+    const { name, description }: Partial<ServerDTO> = req.body;
+
+    const server = await prisma.server.update({
+      where: {
+        id: Number(id),
+      },
+      data: {
+        name,
+        description,
+      },
+    });
+
+    return res.status(200).json({ server, message: 'success' });
+  } catch (e) {
+    handleApiError(e, res);
+  }
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'GET') {
+    await getServer(req, res);
+  } else if (req.method === 'PUT') {
+    await putServer(req, res);
+  } else {
+    res.status(405).json({ message: 'Method not allowed' });
+  }
+}
