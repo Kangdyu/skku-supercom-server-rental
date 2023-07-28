96fce51490ba30e2b6229ba3bd46dbb108ef165c
Kangdyu <kangdyu99@gmail.com>
2023-07-28 14:45:38 +0900

    feat: 어드민 서버 예약 테이블에서 예약 날짜 보기 popover 추가

--- src/components/admin/servers/ServerReservationTable/ServerReservationTable.tsx
+++ src/components/admin/servers/ServerReservationTable/ServerReservationTable.tsx
@@ -1,6 +1,17 @@
 import { useReservations } from '@/hooks/useReservations';
 import { formatDate } from '@/lib/date';
-import { ActionIcon, Group, Pagination, Stack, Table, Text, Title, Tooltip } from '@mantine/core';
+import {
+  ActionIcon,
+  Button,
+  Group,
+  Pagination,
+  Popover,
+  Stack,
+  Table,
+  Text,
+  Title,
+  Tooltip,
+} from '@mantine/core';
 import { IconFileDots } from '@tabler/icons-react';
 import Link from 'next/link';
 import { useState } from 'react';
@@ -23,8 +34,14 @@ export function ServerReservationtable({ serverId }: ServerReservationtableProps
         <thead>
           <tr>
             <th>서버명</th>
-            <th>유저명</th>
-            <th>예약일자</th>
+            <th>예약자</th>
+            <th>이메일</th>
+            <th>전화번호</th>
+            <th>소속대학</th>
+            <th>전공</th>
+            <th>직무</th>
+            <th>예약 희망일</th>
+            <th>예약 생성 일자</th>
             <th>설정</th>
           </tr>
         </thead>
@@ -33,6 +50,23 @@ export function ServerReservationtable({ serverId }: ServerReservationtableProps
             <tr key={reservation.id}>
               <td>{reservation.server.name}</td>
               <td>{reservation.user.name}</td>
+              <td>{reservation.user.email}</td>
+              <td>{reservation.user.phone}</td>
+              <td>{reservation.user.college}</td>
+              <td>{reservation.user.major}</td>
+              <td>{reservation.user.role}</td>
+              <td>
+                <Popover>
+                  <Popover.Target>
+                    <Button size="xs">날짜 보기</Button>
+                  </Popover.Target>
+                  <Popover.Dropdown>
+                    {reservation.reservationDates.map((data) => (
+                      <Text key={data.id}>{formatDate(data.date)}</Text>
+                    ))}
+                  </Popover.Dropdown>
+                </Popover>
+              </td>
               <td>{formatDate(reservation.createdAt)}</td>
               <td>
                 <Group>
