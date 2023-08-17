d037672e64e87ff2712f3483031a9e01a32c7b22
Kangdyu <kangdyu99@gmail.com>
2023-08-17 15:55:13 +0900

    build: docker 설정

--- src/lib/fetcher.ts
+++ src/lib/fetcher.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosClient = axios.create({
-  baseURL: 'http://localhost:3000/api/v1',
+  baseURL: '/api/v1',
 });
 
 export const fetcher = (url: string) => axiosClient.get(url).then((res) => res.data);
